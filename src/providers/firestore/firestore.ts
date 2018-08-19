3b85fb714623d081377de7a3b206fc9af8fa8681
Pritesh R. Patel <tezz995@gmail.com>
2018-08-19 14:01:56 +1200

    Adding to watchlist somewhat working

--- src/providers/firestore/firestore.ts
+++ src/providers/firestore/firestore.ts
@@ -1,23 +1,60 @@
-import { AngularFirestore } from 'angularfire2/firestore';
-import { AuthenticationProvider } from './../authentication/authentication';
-import { UserAccount } from './../../models/userAccount.model';
-import { Injectable } from '@angular/core';
+import {
+    AngularFirestore,
+    AngularFirestoreCollection
+} from "angularfire2/firestore";
+import { AuthenticationProvider } from "./../authentication/authentication";
+import { UserAccount } from "./../../models/userAccount.model";
+import { Injectable } from "@angular/core";
+import { Observable } from "../../../node_modules/rxjs";
 
 @Injectable()
 export class FirestoreProvider {
+    users: Observable<UserAccount[]>;
+    userCollectionRef: AngularFirestoreCollection<UserAccount>;
 
-  constructor(private auth: AuthenticationProvider, private firestore: AngularFirestore) {
-    console.log('Hello FirestoreProvider Provider');
-  }
+    constructor(
+        private auth: AuthenticationProvider,
+        private firestore: AngularFirestore
+    ) {
+        console.log("Hello FirestoreProvider Provider");
 
-  addToWatchlist(movieId: number){
+        this.userCollectionRef = this.firestore.collection("users");
+        this.users = this.userCollectionRef.valueChanges();
+    }
 
-    const userId = this.auth.getUserID(); 
+    // addToWatchlist(movieId: number) {
+    //     this.userCollectionRef
+    //         .doc(this.auth.getUserID())
+    //         .update({ watchlist: watchlist.push(123) });
+    // }
 
-    return this.firestore.doc(`users/${userId}`).set({
-      userId,
-      watchlist: movieId;
-    });
+    addToWatchlist(movieId: number) {
+        const userId = this.auth.getUserID();
 
-  }
+        this.getWatchlist(movieId).then(watchlist => {
+            this.firestore.doc(`users/${userId}`).set({
+                userId,
+                watchlist: [...watchlist, movieId] // Get current watchlist array and append movie id to it.
+            });
+        });
+    }
+
+    getWatchlist(movieId: number) {
+        return new Promise<any[]>((resolve, reject) => {
+            this.firestore
+                .collection("users")
+                .doc(this.auth.getUserID())
+                .ref.get()
+                .then(function(doc) {
+                    if (doc.exists) {
+                        resolve(doc.data().watchlist || []);
+                    } else {
+                        reject("Document not found");
+                    }
+                })
+                .catch(function(error) {
+                    reject("Error getting document: " + error);
+                });
+        });
+    }
 }
